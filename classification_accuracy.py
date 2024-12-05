fcab92a2aec1afee16a1b2d869a7d3e592941fa4
augusjtus <augustus@sjtu.edu.cn>
2024-12-05 14:59:49 +0800

    更改topk_sim_accuracy_meanpearsonr函数的错误

--- classification_accuracy.py
+++ classification_accuracy.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn import metrics
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
+from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
 
 ## Kmeans score
 def Kmeans_score(dyad,VeFC,trn):
@@ -18,6 +21,58 @@ def Kmeans_score(dyad,VeFC,trn):
 
     # print(true_labels,kmeans.labels_)
 
+def KMeans_s_ch_score(VnFC_subj,trn_subj):
+    VnFC_subj_T = VnFC_subj.T
+
+# 进行K-means聚类
+    kmeans = KMeans(n_clusters=trn_subj.shape[1], random_state=42)
+    labels = kmeans.fit_predict(VnFC_subj_T)
+
+# 计算类内离差
+    intra_cluster_distance = 0
+    for i in range(trn_subj.shape[1]):
+        cluster_points = VnFC_subj_T[labels == i]
+        centroid = kmeans.cluster_centers_[i]
+        distances = np.linalg.norm(cluster_points - centroid, axis=1)
+        intra_cluster_distance += np.sum(distances)
+    intra_cluster_distance /= VnFC_subj_T.shape[0]
+    print(f"类内离差: {intra_cluster_distance:.2f}")
+
+# 计算类间离差
+    inter_cluster_distance = 0
+    centroids = kmeans.cluster_centers_
+    for i in range(trn_subj.shape[1]):
+        for j in range(i+1, trn_subj.shape[1]):
+            distance = np.linalg.norm(centroids[i] - centroids[j])
+            inter_cluster_distance += distance
+    inter_cluster_distance /= trn_subj.shape[1]
+    print(f"类间离差: {inter_cluster_distance:.2f}")
+
+# 使用其他指标评估聚类效果
+    silhouette = silhouette_score(VnFC_subj_T, labels)
+    calinski_harabasz = calinski_harabasz_score(VnFC_subj_T, labels)
+    print(f"轮廓系数: {silhouette:.2f}")
+    print(f"Calinski-Harabasz指数: {calinski_harabasz:.2f}")
+
+## Agg_cluster score
+# 创建层次聚类对象，设置聚类的类别数量
+def Agg_cluster_score(VnFC_subj,trn_subj):
+    n_clusters = trn_subj.shape[1]
+    clustering = AgglomerativeClustering(n_clusters=n_clusters)
+
+# 执行层次聚类
+    clusters = clustering.fit_predict(VnFC_subj)
+
+# 假设前面已经有了data（数据向量）和clusters（聚类结果）
+    silhouette_avg = silhouette_score(VnFC_subj, clusters)
+    print("Silhouette Coefficient:", silhouette_avg)
+
+# 绘制聚类结果
+    plt.scatter(VnFC_subj[:, 0], VnFC_subj[:, 1], c=clusters)
+    plt.title("Hierarchical Clustering Results")
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
+    plt.show()
 
 ## topK accuracy
 def get_true_labels(trn):
@@ -161,7 +216,7 @@ def topk_sim_accuracy_meanVFC(VFC,trn,k):
 
     return acrcy,pearsonr_topk_idx_list
 
-def get_pearsonr_subj_mean(pearsonr_list,trn_subj):
+def get_pearsonr_subj_mean(pearsonr_list,trn_subj,trl_idx0):
     trn_pre = 0 
     pearsonr_subj_mean = []
     for trn in trn_subj[0]:
@@ -169,7 +224,11 @@ def get_pearsonr_subj_mean(pearsonr_list,trn_subj):
         pre_idx,post_idx=trn_pre,trn_pre+trn
     # 提取自相关系数矩阵
         pearsonr_subji=pearsonr_list[pre_idx:post_idx]
-        pearsonr_subji_mean = np.mean(pearsonr_subji)
+        # 如果trl_idx0在pre_idx:post_idx之间，即排序为trl_idx0的vfc和自己的相关，数值为1
+        if pre_idx<= trl_idx0 & trl_idx0<=post_idx:
+            pearsonr_subji_mean = (np.sum(pearsonr_subji)-1)/(len(pearsonr_subji)-1)
+        else:
+            pearsonr_subji_mean = np.mean(pearsonr_subji)
         pearsonr_subj_mean.append(pearsonr_subji_mean)
         trn_pre+=trn #记录前一个场景的trail number
 
@@ -208,8 +267,40 @@ def topk_sim_accuracy_meanpearsonr(VFC,trn,k):
 
     return acrcy,pearsonr_topk_idx_list
 
+# binary classification accuracy
+def topk_sim_accuracy_meanpearsonr1(VFC,trn,k):
+    
+    # 计算真实标签
+    true_labels = get_true_labels(trn)
+    true_mean_labels = np.array(range(trn.shape[1]))
+
+    pearsonr_topk_idx_list = []
+    for trl_idx0 in range(VFC.shape[1]):
+        pearsonr_list = []
+        for trl_idx1 in range(VFC.shape[1]):
+            pearsonr = np.corrcoef(VFC[:,trl_idx0],VFC[:,trl_idx1])[0,1]
+            pearsonr_list.append(pearsonr)
+        pearsonr_subj_mean = get_pearsonr_subj_mean(pearsonr_list,trn,trl_idx0)
+        pearsonr_topk_idx,pearsonr_topk_value = get_topk_idx(pearsonr_subj_mean,k)
+        # print(pearsonr_topk_value)
+        if trl_idx0==0:
+            pearsonr_topk_idx_list=pearsonr_topk_idx
+        else:
+            pearsonr_topk_idx_list=np.c_[pearsonr_topk_idx_list,pearsonr_topk_idx]
+
+    # 比较标签，数值为0表示相等
+    labels_comp = np.ones(true_labels.shape)
+    for i in range(k):
+        labels_comp_i = true_mean_labels[list(pearsonr_topk_idx_list[i,:])]-true_labels
+        labels_comp *= labels_comp_i
+
+# 计算正确的比例
+    acrcy = np.count_nonzero(labels_comp==0)/len(labels_comp)
+
+    return acrcy,pearsonr_topk_idx_list
+
+## show middle results
 # topK accuracy with subject_mean pearsonr
-# show middle results
 def topk_sim_accuracy_meanpearsonr_middle_results(VFC,trn,k):
     
     # 计算真实标签
